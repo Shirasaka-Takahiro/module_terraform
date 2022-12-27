c6a074527c58a64bd1511f541a1f685ee0da332f
Shirasaka-Takahiro <kenshirou913@gmail.com>
2022-12-28 04:13:15 +0900

    edited file

--- EC2.tf
+++ EC2.tf
@@ -1,5 +1,64 @@
 ##EC2
-resource "aws_instance" "EC2_01" {
-  ami = var.ami
+resource "aws_instance" "ec2_01" {
+  ami       = var.ami
   subnet_id = var.public_subnet_1a
+  vpc_security_group_id = [
+    aws_security_group.common.id,
+    aws_security_group.ec2.id
+  ]
+  key_name      = aws_key_pair.key.id
+  instance_type = var.instance_type
+  root_block_device {
+    volue_type  = var.volume_type
+    volume_size = var.volume_size
+  }
+
+  tags = {
+    Name = "${var.general_config["project"]}-${var.general_config["env"]}-web01"
+  }
+}
+
+##EC2
+resource "aws_instance" "ec2_02" {
+  ami       = var.ami
+  subnet_id = var.public_subnet_1a
+  vpc_security_group_id = [
+    aws_security_group.common.id,
+    aws_security_group.ec2.id
+  ]
+  key_name      = aws_key_pair.key.id
+  instance_type = var.instance_type
+  root_block_device {
+    volue_type  = var.volume_type
+    volume_size = var.volume_size
+  }
+
+  tags = {
+    Name = "${var.general_config["project"]}-${var.general_config["env"]}-web02"
+  }
+}
+
+##Elastic IP
+resource "aws_eip" "eip_ec2_01" {
+  vpc      = true
+  instance = aws_instance.ec2_01.id
+
+  tags = {
+    Name = "${var.general_config["project"]}-${var.general_config["env"] - eip-web01}"
+  }
+}
+
+resource "aws_eip" "eip_ec2_02" {
+  vpc      = true
+  instance = aws_instance.ec2_02.id
+
+  tags = {
+    Name = "${var.general_config["project"]}-${var.general_config["env"]}-eip-web02"
+  }
+}
+
+##Key Pair
+resource "aws_key_pair" "key" {
+  key_name   = var.key_name
+  public_key = file("~/.ssh/example.pub")
 }
